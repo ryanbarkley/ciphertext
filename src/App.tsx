6cd6435d90a9fc7f75fada7d9935fde06c127479
Ryan Barkley <ryan@ryanbarkley.com>
2021-01-12 17:32:37 -0600

    Configure Lighthouse CI and fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import type {} from "@material-ui/lab/themeAugmentation";
 import { purple, pink, blueGrey } from "@material-ui/core/colors";
 import Box from "@material-ui/core/Box";
 import TextEncodingForm from "./TextEncodingForm";
@@ -16,6 +17,23 @@ const theme = createMuiTheme({
       main: pink[500],
     },
   },
+  overrides: {
+    MuiFormLabel: {
+      root: {
+        color: "rgba(0, 0, 0, 0.75)",
+      },
+    },
+    MuiToggleButton: {
+      root: {
+        "&$selected": {
+          color: "rgba(0, 0, 0, 0.75)",
+        },
+      },
+      label: {
+        color: "rgba(0, 0, 0, 0.65)",
+      },
+    },
+  },
 });
 
 function App() {
@@ -29,9 +47,15 @@ function App() {
         bgcolor={blueGrey[50]}
       >
         <CssBaseline />
-        <AppMeta display="flex" flexDirection="column" marginBottom={3} />
-        <TextEncodingForm />
+        <AppMeta
+          component="header"
+          display="flex"
+          flexDirection="column"
+          marginBottom={3}
+        />
+        <TextEncodingForm component="main" />
         <AppFooter
+          component="footer"
           display="flex"
           justifyContent="space-between"
           marginTop="auto"
--- src/TextEncodingForm.tsx
+++ src/TextEncodingForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import Box, { BoxProps } from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import ListSubheader from "@material-ui/core/ListSubheader";
@@ -14,7 +14,7 @@ import {
   isSupportedAlgorithm,
 } from "./Algorithms";
 
-function TextEncodingForm() {
+function TextEncodingForm(props: BoxProps) {
   const [
     selectedAlgorithm,
     setSelectedAlgorithm,
@@ -48,44 +48,45 @@ function TextEncodingForm() {
   };
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Box display="flex">
+    <Box display="flex" {...props}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
           <Box display="flex">
-            <TextField
-              id="algorithim-selection"
-              label="Algorithm"
-              value={selectedAlgorithm}
-              select
-              variant="filled"
-              onChange={handleSelectedAlgorithmChange}
-              color="primary"
-            >
-              <ListSubheader>Basic Encoding</ListSubheader>
-              <MenuItem value={"base64"}>Base 64</MenuItem>
-              <MenuItem value={"hex"}>Hexadecimal</MenuItem>
-              <MenuItem value={"octal"}>Octal</MenuItem>
-              <ListSubheader>Substitution Ciphers</ListSubheader>
-              <MenuItem value={"caesar"}>Caesar</MenuItem>
-              <MenuItem value={"rot13"}>ROT13</MenuItem>
-            </TextField>
-          </Box>
-          <Box marginLeft={3} display="flex">
-            <ToggleButtonGroup
-              value={algorithmMode}
-              exclusive
-              aria-label="Algorithim Mode"
-              onChange={handleAlgorithmModeChange}
-            >
-              <ToggleButton value="encode" aria-label="encode source text">
-                Encode
-              </ToggleButton>
-              <ToggleButton value="decode" aria-label="decode source text">
-                Decode
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Box>
-          {/* <Box marginLeft={3}>
+            <Box display="flex">
+              <TextField
+                id="algorithim-selection"
+                label="Algorithm"
+                value={selectedAlgorithm}
+                select
+                variant="filled"
+                onChange={handleSelectedAlgorithmChange}
+                color="primary"
+              >
+                <ListSubheader>Basic Encoding</ListSubheader>
+                <MenuItem value={"base64"}>Base 64</MenuItem>
+                <MenuItem value={"hex"}>Hexadecimal</MenuItem>
+                <MenuItem value={"octal"}>Octal</MenuItem>
+                <ListSubheader>Substitution Ciphers</ListSubheader>
+                <MenuItem value={"caesar"}>Caesar</MenuItem>
+                <MenuItem value={"rot13"}>ROT13</MenuItem>
+              </TextField>
+            </Box>
+            <Box marginLeft={3} display="flex">
+              <ToggleButtonGroup
+                value={algorithmMode}
+                exclusive
+                aria-label="Algorithim Mode"
+                onChange={handleAlgorithmModeChange}
+              >
+                <ToggleButton value="encode" aria-label="encode source text">
+                  Encode
+                </ToggleButton>
+                <ToggleButton value="decode" aria-label="decode source text">
+                  Decode
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Box>
+            {/* <Box marginLeft={3}>
             <TextField
               id="algorithim-key"
               label="Key"
@@ -93,35 +94,36 @@ function TextEncodingForm() {
               variant="filled"
             />
           </Box> */}
-        </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="source-text"
+            fullWidth
+            label="Source message"
+            multiline
+            variant="filled"
+            value={sourceText}
+            onChange={handleSourceTextChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="transformed-text"
+            fullWidth
+            label={
+              algorithmMode === "encode" ? "Encoded message" : "Decoded message"
+            }
+            multiline
+            variant="filled"
+            value={transformText(sourceText, selectedAlgorithm, algorithmMode)}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          id="source-text"
-          fullWidth
-          label="Source message"
-          multiline
-          variant="filled"
-          value={sourceText}
-          onChange={handleSourceTextChange}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          id="transformed-text"
-          fullWidth
-          label={
-            algorithmMode === "encode" ? "Encoded message" : "Decoded message"
-          }
-          multiline
-          variant="filled"
-          value={transformText(sourceText, selectedAlgorithm, algorithmMode)}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
 
